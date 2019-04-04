8ea34a8b01f386242932301562f586ffe1971b3b
jbrown25 <justin.brown345@gmail.com>
2019-04-03 19:20:54 -0700

    page heading class/styles, widget heading styles, h1 font

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -12,7 +12,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php if(!is_front_page()) : ?>
-			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+			<?php the_title( '<h1 class="entry-title page_heading">', '</h1>' ); ?>
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
