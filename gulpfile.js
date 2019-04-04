e180966894f712da20d155cdab9e8bf08a2007ba
jbrown25 <justin.brown345@gmail.com>
2019-04-03 18:40:12 -0700

    reinstalled gulp, babel library, rewrote gulpfile for gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,30 @@
-const gulp = require('gulp'),
-      plumber = require('gulp-plumber'),
-      rename = require('gulp-rename');
+const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 const babel = require('gulp-babel');
-const sass = require('gulp-sass');
-
-
-gulp.task('styles', () => {
-  gulp.src(['src/scss/**/*.scss'])
-    .pipe(plumber({
-      errorHandler: error => {
-        console.log(error.message);
-        this.emit('end');
-    }}))
-    .pipe(sass())
-    .pipe(autoprefixer('last 2 versions'))
-    .pipe(gulp.dest('css/'))
-});
+const sass = require('gulp-sass')
 
 gulp.task('scripts', () => {
   return gulp.src('src/js/**/*.js')
-    .pipe(plumber({
-      errorHandler: error => {
-        console.log(error.message);
-        this.emit('end');
-    }}))
     .pipe(babel({
-      presets: ['env']
+      presets: ['@babel/env']
     }))
     .pipe(gulp.dest('js/'))
 });
 
+sass.compiler = require('node-sass');
+ 
+gulp.task('styles', function () {
+  return gulp.src('./src/scss/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+    }))
+    .pipe(gulp.dest('./css'));
+});
+
 gulp.task('default', () => {
-  gulp.watch("src/scss/**/*.scss", ['styles']);
-  gulp.watch("src/js/**/*.js", ['scripts']);
+  gulp.watch('./src/scss/**/*.scss', gulp.series('styles')),
+  gulp.watch('./src/js/**/*.js', gulp.series('scripts'));
+  return;
 });
